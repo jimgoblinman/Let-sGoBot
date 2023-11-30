b4b9fde7ff44bb3d14b5f938c7fd789673155ae7
test1 <jvi135606>
2023-11-30 15:39:00 +0100

    Minor bug fixes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express=require('express')
 const body_parser = require("body-parser")
 const axios = require('axios')
-require
 
 const app=express().use(body_parser.json())
 const token = process.env.TOKEN
@@ -17,10 +16,8 @@ app.get("/webhook", (req, res)=> {
     let challenge=req.query["hub.challenge"]
     let token=req.query["hub.verify_token"]
 
-    const mytoken="";
-
     if(mode && token) {
-        if (mode==="subsribe" && token===mytoken) {
+        if (mode==="subscribe" && token===mytoken) {
             res.status(200).send(challenge)
         } else {
             res.status(403)
@@ -31,22 +28,31 @@ app.get("/webhook", (req, res)=> {
 //post request for sending message
 app.post("/webhook", (req, res) =>{
     let body_param=req.body
-
+    
     console.log(JSON.stringify(body_param,null,2))
+    console.log("Post excecuted")
 
     if(body_param.object) {
-        if(body_param.entry && 
-        body_param.entry[0].changes[0] &&
-        body_param.entry[0].changes[0].value.message && 
-        body_param.entry[0].changes[0].value.message[0]) {
+        console.log("Test")
+        if (
+            body_param.entry &&
+            body_param.entry[0].changes[0] &&
+            body_param.entry[0].changes[0].value.messages &&
+            body_param.entry[0].changes[0].value.messages[0]
+        ) {
+            console.log("Test2")
+            let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = body_param.entry[0].changes[0].value.messages[0].from;
+            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-            let phone_no_id = body.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body.entry[0].changes[0].value.messages[0].text.body;
             
+            console.log("phone_no_id:", phone_no_id);
+            console.log("from:", from);
+            console.log("msg_body:", msg_body)
+
             axios ({
-                metod:"POST",
-                url:"https://graph.facebook.com/v13.0"+phone_no_id+"/message?access_token="+token,
+                method:"POST",
+                url:"https://graph.facebook.com/v17.0"+phone_no_id+"/messages?access_token="+token,
                 data:{
                     messaging_product:"whatsapp",
                     to:from,
