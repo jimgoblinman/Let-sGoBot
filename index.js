f519ff0ca3b44e47bbd8087f8f018bc480242714
test1 <jvi135606>
2023-12-01 17:08:04 +0100

    Added functionalities but still have some problems in the code

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express=require('express')
-const body_parser = require("body-parser")
-const axios = require('axios')
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 require('dotenv').config();
 
-const app = express().use(body_parser.json());
+const app = express().use(bodyParser.json());
 const token = process.env.TOKEN;
 const mytoken = process.env.MYTOKEN;
 
 let out_msg = "Whats up?";
 let name = "";
 let msg = "";
+let questionAsked = false;
+let nthQuestion = false;
 
 app.listen(8000 || process.env.PORT, () => {
     console.log("Webhook is listening");
@@ -57,70 +59,116 @@ app.post("/webhook", (req, res) => {
     }
 });
 
-function main(phone_no_id, from, msg) {
+async function main(phone_no_id, from, msg) {
+    
     if (msg.toLowerCase() === 'start') {
-        console.log("Let's Go");
-        fetchQuestionAndHandle(phone_no_id, from);
-    } else {
+        console.log("Waiting fetch");
+        questionAsked = true;
+        await fetchQuestionAndHandle(phone_no_id, from);
+    } else if (msg.toLowerCase() === "help") {
+        out_msg = "Welcome to the Trivia Bot!\n\n" +
+                  "To start the trivia, type 'start'.\n" +
+                  "The bot will provide you with a question and options.\n" +
+                  "Reply with the number corresponding to the correct answer.\n" +
+                  "For example, if the correct answer is option 2, type '2'.\n" +
+                  "The bot will let you know if you are correct and ask if you want to continue.\n" +
+                  "Enjoy the trivia!";
+        sendWhatsAppMessage(phone_no_id, from, out_msg);
+    } else if (questionAsked == false) {
         out_msg = "Please enter 'start' to start. Type 'help' for a quick start of the Bot";
         sendWhatsAppMessage(phone_no_id, from, out_msg);
     }
 }
 
-function fetchQuestionAndHandle(phone_no_id, from) {
-    fetchQuestion()
-        .then(data => {
-            const results = data.results || [];
-            const questions = results.map(questionData => questionData.question);
-            const incorrectAnswers = results.map(multipleChoice => multipleChoice.incorrect_answers || []);
-            const correctAnswer = results.map(answerData => answerData.correct_answer);
+async function fetchQuestionAndHandle(phone_no_id, from) {
+    try {
+        const data = await fetchQuestion();
+        const results = data.results || [];
+        const questions = results.map(questionData => questionData.question);
+        const incorrectAnswers = results.map(multipleChoice => multipleChoice.incorrect_answers || []);
+        const correctAnswer = results.map(answerData => answerData.correct_answer);
 
-            out_msg = `1. Question: ${questions[0]}\n`;
-            const answers = incorrectAnswers[0].concat(correctAnswer[0]);
-            shuffleArray(answers);
+        out_msg = `1. Question: ${questions[0]}\n`;
+        const answers = incorrectAnswers[0].concat(correctAnswer[0]);
+        shuffleArray(answers);
 
-            // Display options with numbers
-            answers.forEach((option, index) => {
-                out_msg += `${index + 1}. ${option}\n`;
-            });
-
-            // Send the question to WhatsApp
-            sendWhatsAppMessage(phone_no_id, from, out_msg);
-
-            // Wait for the user's response
-            // You can use a delay or implement a mechanism to wait for user input from WhatsApp
-            // For simplicity, let's assume user input is received instantly
-            // For a real application, you would need to implement proper handling of user input
-            // and continue the flow accordingly.
-            // Example: Listen for user input from WhatsApp and call a function to handle the response.
-
-            // Assuming user response is received instantly after sending the question
-            out_msg = "Enter the number corresponding to the correct answer: ";
-            sendWhatsAppMessage(phone_no_id, from, out_msg);
-
-            // Assuming user response is received instantly after sending the question
-            out_msg = "Enter the number corresponding to the correct answer: ";
-            sendWhatsAppMessage(phone_no_id, from, out_msg);
-            let userResponseIndex = msg
-            const userChoiceIndex = parseInt(userResponseIndex) - 1;
-
-            if (answers[userChoiceIndex] === correctAnswer[0]) {
-                out_msg = 'You are correct!\n';
-            } else {
-                out_msg = `Incorrect! The correct answer is: ${correctAnswer[0]}\n`;
-            }
-
-            // Send the result to WhatsApp
-            sendWhatsAppMessage(phone_no_id, from, out_msg);
-
-            // Prompt the user to continue
-            out_msg = "Would you like to continue? (y/n): ";
-            sendWhatsAppMessage(phone_no_id, from, out_msg);
-        })
-        .catch(error => {
-            console.error(error);
+        // Display options with numbers
+        answers.forEach((option, index) => {
+            out_msg += `${index + 1}. ${option}\n`;
         });
+
+        // Send the question to WhatsApp
+        sendWhatsAppMessage(phone_no_id, from, out_msg);
+        questionAsked = true;
+        await delay (500)
+        msg = ''
+        // Wait for the user's response
+        await delay(5000)
+        waitForUserResponse(phone_no_id, from, answers, correctAnswer[0]);
+        console.log("test")
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+async function waitForUserResponse(phone_no_id, from, answers, correctAnswer) {
+    let userResponseIndex;
+
+    // Prompt the user for input
+    //out_msg = "Enter the number corresponding to the correct answer (1-4): ";
+    //sendWhatsAppMessage(phone_no_id, from, out_msg);
+
+    // Wait for the user's response asynchronously
+   
+    // Validate user input
+    userResponseIndex = await msg.trim();
+    msg = '';  // Empty the msg variable for the next input
+
+    //Write here chatgpt
+
+    
+    
+    if (!isInteger(userResponseIndex) || parseInt(userResponseIndex) < 1 || parseInt(userResponseIndex) > 4) {
+        // Send an error message for invalid input
+        out_msg = "Wrong Input pls enter a number between 1 and 4: ";
+        sendWhatsAppMessage(phone_no_id, from, out_msg);
+        delay(500);
+        return;
+    } else {
+
+    const userChoiceIndex = parseInt(userResponseIndex) - 1;
+
+    console.log("User Choice"+userChoiceIndex)
+    console.log("correct answer"+ correctAnswer)
+
+    if (answers[userChoiceIndex] === correctAnswer) {
+        console.log("True");
+        out_msg = 'You are correct!\n';
+    } else {
+        console.log("False");
+        out_msg = `Incorrect! The correct answer is: ${correctAnswer}\n`;
+    }
+
+    // Send the result to WhatsApp
+    sendWhatsAppMessage(phone_no_id, from, out_msg);
+    await delay(1000);
+
+    // Prompt the user to continue
+    out_msg = "Would you like to continue? (y/n): ";
+    sendWhatsAppMessage(phone_no_id, from, out_msg);
+    await delay(5000);
+    if (msg.toLowerCase == 'y' || msg.toLowerCase == 'yes') {
+        letsGo();
+    } else {
+        out_msg = "See ya next time!";
+        sendWhatsAppMessage(phone_no_id, from, out_msg);
+        await delay(500);
+    }
+    questionAsked = false;
+    await delay(500);
+    }
+}
+
 
 function sendWhatsAppMessage(phone_no_id, to, body) {
     axios({
@@ -159,6 +207,20 @@ function shuffleArray(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+}
+
+function isInteger(value) {
+    return /^\d+$/.test(value);
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function letsGo() {
+    console.log("Waiting fetch");
+    questionAsked = true;
+    await fetchQuestionAndHandle(phone_no_id, from);
 }
 
 module.exports = {
